31e99eb59327269edddac4a94dd48b426da9312c
Arturo Paredes <arturojp18@gmail.com>
2022-08-27 09:02:37 -0300

    Alpha blog first commit with home and about page

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  # * We sent the root route to the application controller
+  # *in this case to the pages controller created by the 
+  # *command rails generate pages controller 
+
+  root 'pages#home'
+  # ?About route with get request to the about page
+  # ? static about pages action (#) in pages controller 
+  get 'about', to: 'pages#about'
+
+end
